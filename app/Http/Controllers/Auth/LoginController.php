fb0f4101f972005b2dbd75a760b551a3915b3fbe
Lais Frigério <laaisfrigerio@gmail.com>
2018-11-14 20:50:47 -0200

    Add vote controls css to question and answer item
    Signed-off-by: Lais Frigério <laaisfrigerio@gmail.com>

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/questions';
 
     /**
      * Create a new controller instance.
